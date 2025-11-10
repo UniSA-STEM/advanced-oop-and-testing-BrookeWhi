33bcf1bd7123acad8a3f99fc253b7a6ed9c1cece
Brooke Whitmore <whiby031@mymail.unisa.edu.au>
2025-11-10 17:00:50 +1100

    Base Animal Class Setup
    1.  Animal class created with init and string function
    2.  Child animal classes created for animal category

--- animal.py
+++ animal.py
@@ -1,8 +1,49 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: animal.py
+Description: Animal Class Module
+Author: Brooke Whitmore
+ID: 110468647
+Username: WHIBY031
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Animal:
+    def __init__(self, name, species, age, diet):
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__diet = diet
+        self.__health_issues = []
+        self.__under_treatment = False
+        self.__health_status = "Healthy"
+
+    def __str__(self):
+        return (f"Animal Information \n"
+                f"---------------------\n"
+                f"Name: {self.__name} \n"
+                f"Species: {self.__species} \n"
+                f"Age: {self.__age} \n"
+                f"Diet: {self.__diet} \n")
+
+class Mammal(Animal):
+    def __init__(self, name, species, age, diet):
+        Animal.__init__(self, name, species, age, diet)
+        self.__category = "Mammal"
+
+class Bird(Animal):
+    def __init__(self, name, species, age, diet):
+        Animal.__init__(self, name, species, age, diet)
+        self.__category = "Bird"
+
+class Reptile(Animal):
+    def __init__(self, name, species, age, diet):
+        Animal.__init__(self, name, species, age, diet)
+        self.__category = "Reptile"
+
+
+# TODO:
+#     - add functions
+#         > making sounds
+#         > eating
+#         > sleeping
+#     - health record - (functionality)
