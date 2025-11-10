4fd1f398c7311c77a4deec7db56d8ba9fdb32d43
Brooke Whitmore <whiby031@mymail.unisa.edu.au>
2025-11-10 17:20:04 +1100

    Base Enclosure Class Setup
    1.  Enclosure class created with init and string function

--- enclosure.py
+++ enclosure.py
@@ -1,8 +1,23 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: enclosure.py
+Description: Enclosure Class Module
+Author: Brooke Whitmore
+ID: 110468647
+Username: WHIBY031
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Enclosure:
+    def __init__(self, size, environment_type, animal_type):
+        self.__size = size
+        self.__environment_type = environment_type
+        self.__animals = []
+        self.__animal_type = animal_type
+        self.__cleanliness_level = 100
+
+    def __str__(self):
+        return (f"{self.__animal_type} Enclosure \n"
+                f"-----------------------\n"
+                f"Cleanliness Level: {self.__cleanliness_level}\n"
+                f"Animals: {self.__animals}\n"
+            )
