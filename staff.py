7dd4b2bb224c804687fac68039afbc119881147b
Brooke Whitmore <whiby031@mymail.unisa.edu.au>
2025-11-11 19:02:06 +1100

    Base staff class
    1. added role classes inheriting from staff class
    2. added role specific task functions

--- staff.py
+++ staff.py
@@ -1,8 +1,35 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: staff.py
+Description: Staff Class Module
+Author: Brooke Whitmore
+ID: 110468647
+Username: WHIBY031
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Staff:
+    def __init__(self, name):
+        self.name = name
+
+
+
+class Zookeeper(Staff):
+    def __str__(self):
+        return f"{self.name} - Zookeeper"
+
+    def clean_enclosure(self, enclosure):
+        enclosure.clean()
+
+    def feed_animal(self, animal):
+        animal.feed()
+
+class Veterinarian(Staff):
+    def __str__(self):
+        return f"{self.name} - Veterinarian"
+    def get_name(self):
+        return self.name
+
+    def checkup(self, animal):
+        print(f"{self.name} conducting checkup on {animal.name}")
+        animal.health_check()
+
